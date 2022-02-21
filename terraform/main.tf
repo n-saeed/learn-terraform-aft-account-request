bd9430acabab995fa75f7c92c3da596187f58111
n-saeed <98604075+n-saeed@users.noreply.github.com>
2022-02-21 12:14:32 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+  
+module "dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft.controltower+dev@gmail.com"
+    AccountName               = "Ded"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "aft.controltower+aft@gmail.com"
+    SSOUserFirstName          = "Dev"
+    SSOUserLastName           = "Ops"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "DEV"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "dev"
+}
